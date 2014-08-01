935c94955c4334820ebbe86af552f619d6b45740
Richard Willis <willis.rh@gmail.com>
2014-08-01 13:41:33 +0100

    [ORB-320] Change event type name

---
+++ migrations/m140801_122208_change_event_type_name.php
@@ -0,0 +1,20 @@
+<?php
+
+class m140801_122208_change_event_type_name extends CDbMigration
+{
+	public function up()
+	{
+		$this->update('event_type',
+			array('name' => 'Operation note'),
+			"class_name = 'OphTrOperationnote'"
+		);
+	}
+
+	public function down()
+	{
+		$this->update('event_type',
+			array('name' => 'Operation note'),
+			"class_name = 'Operation Note'"
+		);
+	}
+}
